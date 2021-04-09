6354169397157f5a6892a91d58c8b5bf89591218
bens1320 <yu1piao09@163.com>
2021-04-09 13:39:07 +0800

    完成手写 bind

--- 4-JavaScriptAdvanced/HandWriteBind/src/index.js
+++ 4-JavaScriptAdvanced/HandWriteBind/src/index.js
@@ -1,27 +1,37 @@
-var slice = Array.prototype.slice
-function bind(asThis) {
-  // this 就是函数
-  var args = slice.call(arguments, 1)
-  var fn = this
-  if (typeof fn !== 'function') {
-    throw new Error('bind 必须调用在函数身上')
-  }
-  return function () {
-    var arg2 = slice.call(arguments, 0)
-    return fn.apply(asThis, args.concat(arg2))
-  }
-}
-// es 6写法：既然不支持bind，因此很多 es6 语法糖也就不能用，推荐使用上面这种兼容性好。
-// function bind(asThis, ...args) {
+// var slice = Array.prototype.slice
+// function bind(asThis) {
 //   // this 就是函数
-//   const fn = this
-//   return function (...arg2) {
-//     return fn.call(asThis, ...args, ...arg2)
+//   var args = slice.call(arguments, 1)
+//   var fn = this
+//   if (typeof fn !== 'function') {
+//     throw new Error('bind 必须调用在函数身上')
+//   }
+//   return function () {
+//     var args2 = slice.call(arguments, 0)
+//     return fn.apply(asThis, args.concat(args2))
 //   }
 // }
 
-module.exports = bind
-
-if (!Function.prototype.bind()) {
-  Function.prototype.bind = bind()
+// es 6写法：既然不支持 bind，因此很多 es6 语法糖也就不能用，推荐使用上面这种兼容性好。
+function _bind(asThis, ...args) {
+  // this 就是函数
+  const fn = this
+  function ResFn(...args2) {
+    return fn.call(
+      this instanceof ResFn ? this : asThis,
+      ...args,
+      ...args2
+    )
+  }
+  ResFn.prototype = fn.prototype
+  return  ResFn
 }
+
+module.exports = _bind
+
+if (!Function.prototype.bind) {
+  Function.prototype.bind = _bind()
+}
+
+
+
--- 4-JavaScriptAdvanced/HandWriteBind/test/index.js
+++ 4-JavaScriptAdvanced/HandWriteBind/test/index.js
@@ -8,27 +8,18 @@ const bind = require('../src/index')
 Function.prototype.bind2 = bind
 
 describe('bind', () => {
-  it('bind 不为 undefined', () => {
+  it('1 bind 不为 undefined', () => {
     assert(Function.prototype.bind2 !== undefined)
   })
 
-  it('接收 this', () => {
+  it('2 接收 this', () => {
     const fn1 = function () {
       return this
     }
     const newFn1 = fn1.bind2({name: 'ben'})
     assert(newFn1().name === 'ben')
   })
-  it('接收 this，同时接收其他参数', () => {
-    const fn1 = function (p1, p2) {
-      return [this, p1, p2]
-    }
-    const newFn1 = fn1.bind2({name: 'ben'}, 123, 456)
-    assert(newFn1()[0].name === 'ben')
-    assert(newFn1()[1] === 123)
-    assert(newFn1()[2] === 456)
-  })
-  it('接收 this，同时两次传参', ()=>{
+  it('3 接收 this，同时两次传参', () => {
     const fn1 = function (p1, p2) {
       return [this, p1, p2]
     }
@@ -37,7 +28,59 @@ describe('bind', () => {
     assert(newFn1(223)[1] === 123)
     assert(newFn1(223)[2] === 223)
   })
+  it('4 接收 this，同时接收其他参数', () => {
+    const fn1 = function (p1, p2) {
+      return [this, p1, p2]
+    }
+    const newFn1 = fn1.bind2({name: 'ben'}, 123, 456)
+    assert(newFn1()[0].name === 'ben')
+    assert(newFn1()[1] === 123)
+    assert(newFn1()[2] === 456)
+  })
+  it('5 new 的时候绑定了 p1, p2', () => {
+    Function.prototype.bind2 = bind
+    const fn = function (p1, p2) {
+      this.p1 = p1
+      this.p2 = p2
+    }
+    const fn2 = fn.bind2(undefined, 'x', 'y')
+    const object = new fn2()
+    assert(object.p1 === 'x')
+    assert(object.p2 === 'y')
+  })
+  it('6 new 的时候绑定了 p1, p2, 并且 fn 有 prototype.sayHi', () => {
+    const fn = function (p1, p2) {
+      this.p1 = p1
+      this.p2 = p2
+    }
+    fn.prototype.sayHi = function () {}
+    const fn2 = fn.bind2(undefined, 'x', 'y')
+    const object = new fn2()
+    assert(object.p1 === 'x')
+    assert(object.p2 === 'y')
+    assert(object.__proto__ === fn.prototype)
+    assert(typeof object.sayHi === 'function')
+  })
+
+  it('7 不用 new，用类似的对象', () => {
+    const fn = function (p1, p2) {
+      this.p1 = p1
+      this.p2 = p2
+    }
+    fn.prototype.sayHi = function () {}
+    const object1 = new fn('a', 'b')
+    console.log(object1)
+    const fn2 = fn.bind2(object1, 'x', 'y')
+    const object = fn2()
+    assert(object === undefined)
+    assert(object1.p1 === 'x')
+    assert(object1.p2 === 'y')
+  })
+
 })
+
+
+
 
 
 
